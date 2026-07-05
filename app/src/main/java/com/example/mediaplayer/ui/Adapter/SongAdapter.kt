df9d9f74646e3ff0e62a79ad38d6d8484ed543a0
Ema0426 <emanuelegaudiano261004@gmail.com>
2026-07-05 11:48:17 +0200

    Aggiunti mimalist, item_song e library xml, giusto per far funzionare il progetto. Aggiunta logica di Adapter per le songs.

--- app/src/main/java/com/example/mediaplayer/ui/Adapter/SongAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.mediaplayer.ui.Adapter
-
-class SongAdapter {
-}
---
+++ app/src/main/java/com/example/mediaplayer/ui/adapter/SongAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.mediaplayer.ui.adapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.mediaplayer.R
+import com.example.mediaplayer.databinding.ItemSongBinding
+import com.example.mediaplayer.model.Song
+
+/*
+*   in sostanza stiamo collegando la classe Song,
+*   con la RecyclerView. quando deve far apparire le canzoni
+*   ne allocherà solo il numero necessario visualizzabile a schermo.
+*   quando scorri la lista, se ne occuperà il GC a deallocare tutto quanto
+*   
+*/
+class SongAdapter (
+    private var songList : List<Song>,
+    private val onSongClick: (Song) -> Unit
+) : RecyclerView.Adapter<SongAdapter.SongViewHolder>(){
+
+    inner class SongViewHolder(private val biding : ItemSongBinding) : RecyclerView.ViewHolder(biding.root){
+        fun bind (song : Song){
+            biding.textViewArtist.text = song.artist
+            biding.textViewTitle.text = song.title
+
+            biding.imageViewCover.load(song.coverUrl){
+                crossfade(true)
+                placeholder(R.drawable.ic_launcher_background)
+                error(R.drawable.ic_launcher_foreground)
+            }
+
+            biding.root.setOnClickListener {
+                onSongClick(song)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        val biding = ItemSongBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SongViewHolder(biding)
+    }
+
+    override fun getItemCount(): Int = songList.size
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        holder.bind(songList[position])
+    }
+
+    fun updateSongs(newList: List<Song>){
+        this.songList = newList
+        notifyDataSetChanged()
+    }
+}
